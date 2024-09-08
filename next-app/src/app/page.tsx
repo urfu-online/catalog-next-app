059530c25651b1ae13d5aedf5e0509c11a422d37
lea_alex <lea_alex@mail.ru>
2024-09-08 13:57:34 +0500

    Категории

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -3,6 +3,8 @@
 import { courses } from '@/src/app/data'
 import { useState } from 'react'
 import Image from 'next/image'
+import { Card, Checkbox, Container, FormControl, FormControlLabel, FormGroup, FormLabel, Paper } from '@mui/material'
+import Grid from '@mui/material/Grid2'
 
 export default function Home() {
   const [list, setList] = useState(courses)
@@ -19,132 +21,147 @@ export default function Home() {
   console.log(list)
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-12">
-      <div className="z-10 w-full max-w-5xl items-center justify-between lg:flex mb-12">
-        <p className="fixed left-0 top-0 flex w-full justify-center from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:from-inherit lg:static lg:w-auto  lg:rounded-xl  lg:p-4 ">
-          <Image src="/urfu_logo.svg" alt="logo" width="180" height="90" />
-        </p>
-      </div>
-
-      {/*<div className="text-2xl my-10">*/}
-      {/*  <h1>Онлайн-курсы УрФУ</h1>*/}
-      {/*</div>*/}
-      <div className="mb-12 max-w-5xl w-full">
-        <label htmlFor="hs-trailing-button-add-on-with-icon-and-button" className="sr-only">
-          Label
-        </label>
-        <div className="relative flex rounded-lg shadow-sm">
-          <input
-            type="text"
-            id="hs-trailing-button-add-on-with-icon-and-button"
-            name="hs-trailing-button-add-on-with-icon-and-button"
-            className="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm rounded-s-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-            placeholder="Введите название курса"
-            onChange={(e) => {
-              setList(courses.filter((element) => element.title.toLowerCase().includes(e.target.value.toLowerCase())))
-            }}
-          />
-          <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
-            <svg
-              className="shrink-0 size-4 text-gray-400"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <circle cx="11" cy="11" r="8"></circle>
-              <path d="m21 21-4.3-4.3"></path>
-            </svg>
-          </div>
-          <button
-            type="button"
-            className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-e-md border border-transparent bg-gray-600 text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 disabled:opacity-50 disabled:pointer-events-none"
-          >
-            Поиск
-          </button>
-        </div>
-      </div>
-
-      <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">Категории</h3>
-      <ul className=" grid text-center  lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left grid-cols-2">
-        {Object.entries(category).map(([name, value]) => (
-          <li key={name} className="w-full dark:border-gray-600">
-            <div className="flex items-center ps-3">
-              <input
-                id={name}
-                type="checkbox"
-                defaultChecked={value}
-                onChange={(e) => {
-                  setCategory({ ...category, [name]: e.target.checked })
-                }}
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              />
-              <label htmlFor={name} className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                {name}
-              </label>
-            </div>
-          </li>
-        ))}
-      </ul>
-
-      <h3 className="my-4 font-semibold text-gray-900 dark:text-white">Онлайн-курсы</h3>
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left">
-        {list
-          .filter((course) =>
-            Object.entries(category)
-              .filter(([_, value]) => value)
-              .some(([tagname, _]) => course.tags[tagname]),
-          )
-          .map((course, index) => (
-            <a
-              href={course.link}
-              className="m-3 group flex flex-col justify-between bg-white rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-              target="_blank"
-              rel="noopener noreferrer"
-              key={course.title + index}
-            >
-              <div className="flex flex-col">
-                {course.platform == 'НПОО' ? (
-                  <div className="mb-2 self-end">
-                    <Image src="/openedu.png" className="inline" alt="НПОО" width={24} height={24} />{' '}
-                    <span className="m-1 text-sm self-end opacity-50">{course.platform}</span>
-                  </div>
-                ) : (
-                  <div className="mb-2 self-end">
-                    <Image className="inline" src="/urfu.png" alt="УрФУ" width={24} height={24} />
-                    <span className="m-1 text-sm self-end opacity-50">{course.platform}</span>
-                  </div>
-                )}
-                <hr />
-
-                <h2 className="mb-3 mt-2 text-md font-semibold">{course.title} </h2>
-                <div className={'mb-8 flex flex-wrap items-center'}>
-                  {Object.entries(course.tags)
-                    .filter(([_, value]) => value)
-                    .map(([tag, _]) => (
-                      <p
-                        key={tag}
-                        className=" m-1 py-1 px-3 text-xs text-blue-800 border border-blue-800 border-solid rounded-lg"
-                      >
-                        {tag}
-                      </p>
-                    ))}
+    <>
+      <Container
+        sx={{
+          py: 2,
+          backgroundColor: 'rgba(214, 219, 220, .6)',
+          position: 'sticky',
+          top: 0,
+          backdropFilter: 'blur(5px)',
+          zIndex: 999,
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid size={3}>
+            <Image src="/urfu_logo.svg" alt="logo" width="160" height="80" />
+          </Grid>
+          <Grid size={9}>
+            <div className="my-1 max-w-5xl w-full">
+              <div className="relative flex rounded-lg shadow-sm">
+                <input
+                  type="text"
+                  id="hs-trailing-button-add-on-with-icon-and-button"
+                  name="hs-trailing-button-add-on-with-icon-and-button"
+                  className="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm rounded-s-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                  placeholder="Введите название курса"
+                  onChange={(e) => {
+                    setList(
+                      courses.filter((element) => element.title.toLowerCase().includes(e.target.value.toLowerCase())),
+                    )
+                  }}
+                />
+                <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
+                  <svg
+                    className="shrink-0 size-4 text-gray-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <circle cx="11" cy="11" r="8"></circle>
+                    <path d="m21 21-4.3-4.3"></path>
+                  </svg>
                 </div>
+                <button
+                  type="button"
+                  className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-e-md border border-transparent bg-gray-600 text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 disabled:opacity-50 disabled:pointer-events-none"
+                >
+                  Поиск
+                </button>
               </div>
-              <p>
-                Подробнее
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                  -&gt;
-                </span>
-              </p>
-            </a>
-          ))}
-      </div>
-    </main>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+      <Container sx={{ pt: 3 }}>
+        <Grid container spacing={2}>
+          <Grid size={3}>
+            <Paper sx={{ position: 'sticky', top: 86 }}>
+              <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+                <FormLabel component="legend">Категории</FormLabel>
+                <FormGroup>
+                  {Object.entries(category).map(([name, value]) => (
+                    <FormControlLabel
+                      key={name}
+                      control={
+                        <Checkbox
+                          checked={value}
+                          onChange={(e) => {
+                            setCategory({ ...category, [name]: e.target.checked })
+                          }}
+                          inputProps={{ 'aria-label': 'controlled' }}
+                        />
+                      }
+                      label={name}
+                    />
+                  ))}
+                </FormGroup>
+              </FormControl>
+            </Paper>
+          </Grid>
+          <Grid size={9} container spacing={2} height={1}>
+            {list
+              .filter((course) =>
+                Object.entries(category)
+                  .filter(([_, value]) => value)
+                  .some(([tagname, _]) => course.tags[tagname]),
+              )
+              .map((course, index) => (
+                <Grid size={4} key={course.title + index}>
+                  <Card sx={{ height: '100%' }}>
+                    <a
+                      href={course.link}
+                      className="group flex flex-col justify-between bg-white  px-5 py-4 transition-colors hover:border-gray-300 hover:bg-indigo-50 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 h-full"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <div className="flex flex-col">
+                        {course.platform == 'НПОО' ? (
+                          <div className="mb-2 self-end">
+                            <Image src="/openedu.png" className="inline" alt="НПОО" width={24} height={24} />{' '}
+                            <span className="m-1 text-sm self-end opacity-50">{course.platform}</span>
+                          </div>
+                        ) : (
+                          <div className="mb-2 self-end">
+                            <Image className="inline" src="/urfu.png" alt="УрФУ" width={24} height={24} />
+                            <span className="m-1 text-sm self-end opacity-50">{course.platform}</span>
+                          </div>
+                        )}
+                        <hr />
+
+                        <h2 className="mb-3 mt-2 text-md font-semibold">{course.title} </h2>
+                        <div className={'mb-8 flex flex-wrap items-center'}>
+                          {Object.entries(course.tags)
+                            .filter(([_, value]) => value)
+                            .map(([tag, _]) => (
+                              <p
+                                key={tag}
+                                className=" m-1 py-1 px-3 text-xs text-blue-800 border border-blue-800 border-solid rounded-lg"
+                              >
+                                {tag}
+                              </p>
+                            ))}
+                        </div>
+                      </div>
+                      <p>
+                        Подробнее{' '}
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                          -&gt;
+                        </span>
+                      </p>
+                    </a>
+                  </Card>
+                </Grid>
+              ))}
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   )
 }
